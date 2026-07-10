0ea8757ddf605760753d1c288530a7c10f1ddfba
dallo <dallo@local>
2026-07-10 08:29:27 +0100

    fix: launch desktop app without console

--- run-desktop.bat
+++ run-desktop.bat
@@ -3,10 +3,10 @@ setlocal
 
 cd /d "%~dp0"
 
-if exist ".venv\Scripts\python.exe" (
-    ".venv\Scripts\python.exe" run.py
+if exist ".venv\Scripts\pythonw.exe" (
+    ".venv\Scripts\pythonw.exe" run.py
 ) else (
-    python run.py
+    pythonw run.py
 )
 
 endlocal
